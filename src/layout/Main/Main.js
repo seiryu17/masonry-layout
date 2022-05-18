b3049131f7198c054145edbed049c9442e60bfda
Rio Riferro Lim <rio@grip-principle.com>
2022-05-18 21:19:36 +0700

    Create the mansory skeleton image and fetch the data

--- src/layout/Main/Main.js
+++ src/layout/Main/Main.js
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./Main.module.css";
+import data from "../../datasource";
 
 const Main = () => {
-  return <div className={styles["main-container"]}>Main</div>;
+  return (
+    <div className={styles["main-container"]}>
+      {data.map((x) => {
+        let temp = x.src.split("/");
+        temp[4] = temp[4].substring(0, 3);
+        x.width = `${temp[3]}px`;
+        x.height = `${temp[4]}px`;
+        return (
+          <img src={x.src} alt={x.name} width={x.width} height={x.height} />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Main;
