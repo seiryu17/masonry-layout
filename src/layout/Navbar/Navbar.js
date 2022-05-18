514bafa0ec69df4b417af26a39425ed000e3a833
Rio Riferro Lim <rio@grip-principle.com>
2022-05-18 20:41:58 +0700

    Create structure folder

---
+++ src/layout/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <input></input>
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+  );
+};
+
+export default Navbar;
