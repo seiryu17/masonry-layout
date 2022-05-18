11b9c92bc91b595cb4e7b80404bec258343a2151
Rio Riferro Lim <rio@grip-principle.com>
2022-05-18 21:32:28 +0700

    add media query for responsiveness

--- src/layout/Main/Main.js
+++ src/layout/Main/Main.js
@@ -5,15 +5,24 @@ import data from "../../datasource";
 const Main = () => {
   return (
     <div className={styles["main-container"]}>
-      {data.map((x) => {
-        let temp = x.src.split("/");
-        temp[4] = temp[4].substring(0, 3);
-        x.width = `${temp[3]}px`;
-        x.height = `${temp[4]}px`;
-        return (
-          <img src={x.src} alt={x.name} width={x.width} height={x.height} />
-        );
-      })}
+      {data
+        .sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
+        .map((x, index) => {
+          console.log(x);
+          let temp = x.src.split("/");
+          temp[4] = temp[4].substring(0, 3);
+          x.width = `${temp[3]}px`;
+          x.height = `${temp[4]}px`;
+          return (
+            <img
+              src={x.src}
+              alt={x.name}
+              width={x.width}
+              height={x.height}
+              key={index}
+            />
+          );
+        })}
     </div>
   );
 };
