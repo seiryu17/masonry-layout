7968601e227b36b1ecc706bcbdcadd67619800d2
Rio Riferro <gulivverio@gmail.com>
2022-05-19 16:28:19 +0700

    Add commenting

--- src/layout/Main/Main.js
+++ src/layout/Main/Main.js
@@ -7,22 +7,29 @@ import Navbar from "../../parts/Navbar/Navbar";
 const Main = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
 
-  let listToDisplay = datasource;
+  // Sort image alphabetical by attribute "NAME" and put it into variable
+  let listToDisplay = datasource.sort((a, b) =>
+    a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+  );
 
+  // To filter item list by user input search
   if (searchTerm !== "") {
     listToDisplay = datasource.filter((fruit) => {
       return fruit.name.includes(searchTerm);
     });
   }
 
+  // Function used to get value on input change
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
+  // Function to give delay for user input for 500ms before doing the filter
   const debouncedResults = useMemo(() => {
     return debouce(handleChange, 500);
   }, []);
 
+  // Useeffect to clean up the debounce
   useEffect(() => {
     return () => {
       debouncedResults.cancel();
@@ -40,23 +47,22 @@ const Main = (props) => {
         }
       >
         {listToDisplay.length > 0
-          ? listToDisplay
-              .sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
-              .map((x, index) => {
-                let temp = x.src.split("/");
-                temp[4] = temp[4].substring(0, 3);
-                x.width = `${temp[3]}px`;
-                x.height = `${temp[4]}px`;
-                return (
-                  <img
-                    src={x.src}
-                    alt={x.name}
-                    width={x.width}
-                    height={x.height}
-                    key={index}
-                  />
-                );
-              })
+          ? listToDisplay.map((x, index) => {
+              let temp = x.src.split("/");
+              temp[4] = temp[4].substring(0, 3);
+              x.width = `${temp[3]}px`;
+              x.height = `${temp[4]}px`;
+              return (
+                <img
+                  src={x.src}
+                  alt={x.name}
+                  width={x.width}
+                  height={x.height}
+                  key={index}
+                  loading="lazy"
+                />
+              );
+            })
           : "Sorry, no items found."}
       </div>
     </>
